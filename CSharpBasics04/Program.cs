83609df1e8e00c81806d6dc9e90847b73cfd3f84
Mustafa Khaled <mustafakh565@gmail.com>
2026-09-05 23:26:24 +0300

    Question02

--- CSharpBasics04/Program.cs
+++ CSharpBasics04/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CSharpBasics04
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CSharpBasics04
 {
     internal class Program
     {
@@ -10,6 +12,19 @@
 
             Console.WriteLine(prices[1]);
             #endregion
+
+            #region Question02
+            // 2. Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies.Print the number of copies on shelf 1, slot 0.
+            int[,] shelfCopies =
+            {
+                { 3, 5 },
+                { 1, 4 }
+            };
+
+            Console.WriteLine(shelfCopies[1, 0]);
+
+            #endregion
+
         }
     }
 }
